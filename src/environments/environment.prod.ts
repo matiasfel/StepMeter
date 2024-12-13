1bc467ef8f4d160903c71b71c9d0b663e6a97421
Matías Baxman <matiasbaxman@gmail.com>
2024-12-12 23:44:40 -0300

    Android Studio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  FirebaseConfig:{
+    apiKey: "AIzaSyCDfWgIxnc3N9t_QHgF_3ir-BD35f-g0_4",
+    authDomain: "stepmeter-a3158.firebaseapp.com",
+    projectId: "stepmeter-a3158",
+    storageBucket: "stepmeter-a3158.appspot.com",
+    messagingSenderId: "285075813445",
+    appId: "1:285075813445:android:803660412983800e485f2c",
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,7 @@
 import { FirebaseLoginService } from "src/app/services/firebaseService/firebase-login.service";
 
 export const environment = {
-  production: false,
-  FirebaseConfig:{
-    apiKey: "AIzaSyCDfWgIxnc3N9t_QHgF_3ir-BD35f-g0_4",
-    authDomain: "stepmeter-a3158.firebaseapp.com",
-    projectId: "stepmeter-a3158",
-    storageBucket: "stepmeter-a3158.appspot.com",
-    messagingSenderId: "285075813445",
-    appId: "1:285075813445:android:803660412983800e485f2c",
-  }
+  production: false
 };  
 
 /*
